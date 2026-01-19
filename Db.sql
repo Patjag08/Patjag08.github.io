ce00b97111c889d2868c0f211109689facb6ceb8
Patryk Jagielski <117273549+Patjag08@users.noreply.github.com>
2026-01-19 10:51:43 +0000

    Add db.sql
    Required for school project

---
+++ Db.sql
@@ -0,0 +1,20 @@
+CREATE TABLE user (
+  user_id INT AUTO_INCREMENT PRIMARY KEY,
+  user_email VARCHAR(255) NOT NULL,
+  user_name VARCHAR(255) NOT NULL,
+  user_password VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE past_Jobs (
+  user_id INT NOT NULL,
+  jobs_json JSON,
+  FOREIGN KEY (user_id) REFERENCES user(user_id)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE
+);
+
+CREATE TABLE staff_Accounts (
+  staff_id INT AUTO_INCREMENT PRIMARY KEY,
+  staff_user VARCHAR(255),
+  staff_password VARCHAR(255)
+);
